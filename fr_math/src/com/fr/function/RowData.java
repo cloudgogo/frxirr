087ab14badd923f735210982fb584b7835265bdd
AllenCharles <cloudman@foxmail.com>
2017-09-11 10:48:08 +0800

    someproject

---
+++ fr_math/src/com/fr/function/RowData.java
@@ -0,0 +1,33 @@
+package com.fr.function;
+
+public class RowData {
+	private int id;
+	private String name;
+	private String description;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
